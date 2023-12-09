814698b54b9e8016dd658d8ed21bffe8d9021b22
Panagiotis Moschos <pan.moschos86@gmail.com>
2023-12-09 23:59:57 +0200

    65th commit

--- 17.screenshot/screenshot.py
+++ 17.screenshot/screenshot.py
@@ -1,13 +1,24 @@
 import time
 import pyautogui
 
+def take_screenshot():
+    """
+    Takes a screenshot of the current screen after a delay.
 
-def screenshot():
-    name = int(round(time.time() * 1000))
-    name = '{}.png'.format(name)  # To name the file
-    time.sleep(5)  # Time Wait Before Taking Screenshot
-    img = pyautogui.screenshot(name)
-    img.show()  # To Show the Screenshot After Being Taken
+    The screenshot is saved with a filename based on the current timestamp.
+    The file is saved in PNG format and then displayed.
+    """
+    # Delay before taking the screenshot to allow for screen setup
+    time.sleep(5)
 
+    # Generating a unique filename using the current timestamp
+    timestamp = int(round(time.time() * 1000))
+    filename = f'{timestamp}.png'
 
-screenshot()
+    # Taking the screenshot
+    screenshot_image = pyautogui.screenshot(filename)
+
+    # Displaying the screenshot
+    screenshot_image.show()
+
+take_screenshot()
